eb6a1715e1b402dbdfa002fe38a39a50d22bed28
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2024-12-30 23:12:48 -0600

    restarting project

--- src/app/workout/current.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "@/components/generic/themed";
-
-export default function CurrentWorkout() {
-  return (
-    <View>
-      <Text>Current work out screen</Text>
-    </View>
-  );
-}
